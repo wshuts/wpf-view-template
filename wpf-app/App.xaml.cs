b01d24d42a64127f3ea67de0ea7a6ca5520178a0
wshuts <shutswilliam@gmail.com>
2024-06-10 23:08:32 -0500

    main app stuff

---
+++ wpf-app/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using wpf_app.ViewModels;
+using wpf_app.Views;
+
+namespace wpf_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainViewModel _mainViewModel;
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            _mainViewModel = new MainViewModel();
+            MainWindow = new MainWindow(_mainViewModel);
+            MainWindow.Show();
+        }
+    }
+}
