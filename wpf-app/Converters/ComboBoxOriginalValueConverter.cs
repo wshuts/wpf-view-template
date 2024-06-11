1b2faebf5986dfced819b5578f1efc1d99117e29
wshuts <shutswilliam@gmail.com>
2024-06-10 23:12:10 -0500

    behaviors, converters, and lookups

---
+++ wpf-app/Converters/ComboBoxOriginalValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using wpf_app.DataProvider.Lookups;
+
+namespace wpf_app.Converters
+{
+  public class ComboBoxOriginalValueConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var id = (int)value;
+      var comboBox = parameter as ComboBox;
+      if(comboBox!=null && comboBox.ItemsSource != null)
+      {
+        var lookupItem 
+          = comboBox.ItemsSource.OfType<LookupItem>().SingleOrDefault(l => l.Id == id);
+        if(lookupItem!= null)
+        {
+          return lookupItem.DisplayValue;
+        }
+      }
+      return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ wpf-app/DataProvider/Lookups/LookupItem.cs
@@ -0,0 +1,9 @@
+namespace wpf_app.DataProvider.Lookups
+{
+  public class LookupItem
+  {
+    public int Id { get; set; }
+
+    public string DisplayValue { get; set; }
+  }
+}
